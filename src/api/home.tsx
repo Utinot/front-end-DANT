57d60db4f1f4651da8a7ddcf3afae8a244b8f0e6
khangle23078 <khangle2307@gmail.com>
2022-10-23 00:02:34 +0700

    final commit

--- src/api/home.tsx
+++ src/api/home.tsx
@@ -1,6 +1,6 @@
 import { isAuthenticate } from "./auth";
-import { httpOptions, instance } from "./instance"
-// const { token } = isAuthenticate();
+import { instance } from "./instance"
+const { token } = isAuthenticate();
 
 export const listCandidate = () => {
     const url = `/index/list`
@@ -15,7 +15,7 @@ export const addNews = (data: any) => {
     return instance.post(url, data)
 }
 export const showNews = () => {
-    const url = `/employer/show/`
+    const url = `/employer/show/${token}`
     return instance.get(url)
 }
 export const removeShowNews = (id: any) => {
@@ -23,14 +23,14 @@ export const removeShowNews = (id: any) => {
     return instance.delete(url)
 }
 export const listNews = () => {
-    const url = `/employer/create`
-    return instance.get(url, httpOptions)
+    const url = `/employer/create/${token}`
+    return instance.get(url)
 }
 export const createNews = (data: any) => {
     const url = `/employer/store`
     return instance.post(url, data)
 }
 export const listNewsemp = () => {
-    const url = `/employer/index`
-    return instance.get(url, httpOptions)
+    const url = `/employer/index/${token}`
+    return instance.get(url)
 }
