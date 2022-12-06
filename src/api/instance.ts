e10d32ce2f24190d9099b83e8ff9afe7c29dc724
Utinot <levanthuan412002@gmail.com>
2022-12-07 01:05:13 +0700

    fix api

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 export const instance = axios.create({
-   baseURL: 'http://datnweb19.herokuapp.com/api',
+   baseURL: 'http://192.168.1.43:8080/api',
    headers: {
       'Content-type': 'application/json',
       'Access-Control-Allow-Origin': '*'
